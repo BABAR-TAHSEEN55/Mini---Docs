1c1c2d7fa7142f3852f92802ce71eed6aee74dd5
Sizzler_55 <babarsmoke24@gmail.com>
2024-11-06 19:30:39 +0530

    Passing Prps

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,25 +1,34 @@
 import React from "react";
 import { FaFileDownload } from "react-icons/fa";
 import { MdDownloadForOffline } from "react-icons/md";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
-function Cards() {
+function Cards({ data }) {
   return (
-    <div className="relative h-48 w-40 bg-zinc-400   p-3 rounded-[30px] overflow-hidden">
+    <div className="relative h-48 w-40 bg-indigo-300   p-3 rounded-[30px] overflow-hidden">
       <FaFileDownload />
-      <p className="text-xs mt-2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </p>
+      <p className="text-xs mt-2">{data.description}</p>
       <div className="footer absolute bottom-8 w-full h-10 left-0    ">
         <div className="flex items-center justify-between px-5 py-2 ">
-          <h5>0.5mB</h5>
+          <h5>{data.fileSize}</h5>
           <span className="">
             {" "}
-            <MdDownloadForOffline className=" mr-2 " size={18} />
+            {data.Close ? (
+              <MdDownloadForOffline className=" mr-2 " size={18} />
+            ) : (
+              <IoCloseCircleOutline className=" mr-2 " size={18} />
+            )}
           </span>
         </div>
-        <div className=" tag w-full bottom-10 h-10 bg-green-600 flex items-center justify-center">
-          <h3 className="font-semibold">Download Now</h3>
-        </div>
+        {data.tag.tagOpen && (
+          <div
+            className={`tag w-full bottom-10 h-10 ${
+              data.tag.tagColor === "blue" ? "bg-blue-400" : "bg-indigo-500"
+            } flex items-center justify-center`}
+          >
+            <h3 className="font-semibold">{data.tag.tagTitle}</h3>
+          </div>
+        )}
       </div>
     </div>
   );
