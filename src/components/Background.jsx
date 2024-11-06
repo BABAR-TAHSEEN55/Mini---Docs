ba942e6c8ffddf263b936dd26da77f89812c597c
Sizzler_55 <babarsmoke24@gmail.com>
2024-11-06 13:40:48 +0530

    Background fix

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Background() {
   return (
     <>
-      <div className="w-full h-screen">
+      <div className="fixed z-[2] w-full h-screen">
         <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] font-semibold text-zinc-700 leading-loose tracking-relaxed">
           Docs{" "}
         </h1>
