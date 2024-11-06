99b1a17fa7b3d660e843eadcd80b1fdf3cd82c7d
Sizzler_55 <babarsmoke24@gmail.com>
2024-11-06 13:41:07 +0530

    ForeGround Added

---
+++ src/components/Foreground.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Cards from "../components/Cards";
+function Foreground() {
+  return (
+    <div className="fixed z-[4] top-0 left-0 w-full h-full ">
+      <Cards />
+    </div>
+  );
+}
+
+export default Foreground;
